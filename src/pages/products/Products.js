3fd2211e18ddbe0e9a8d27de81a828aa46fc26a2
Cihat Turgut <cihatturgut@yandex.com>
2022-12-08 15:36:48 +0300

    created custom hook

--- src/pages/products/Products.js
+++ src/pages/products/Products.js
@@ -1,24 +1,24 @@
-import React, {useState, useEffect} from 'react';
-import {SafeAreaView, FlatList, Text} from 'react-native';
+import React from 'react';
+import {SafeAreaView, FlatList, ActivityIndicator, Text} from 'react-native';
 import ProductCard from '../../components/productcard';
-import axios from 'axios';
 import {API_URL} from '@env';
-import styles from './Products.style';
+import useFetch from '../../hooks/usefetch';
 
 const Products = () => {
-  const [productData, setProductData] = useState([]);
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-  const fetchData = async () => {
-    const {data} = await axios.get(API_URL);
-    setProductData(data);
-  };
+  const {loading, error, response} = useFetch(API_URL);
   const renderProduct = ({item}) => <ProductCard product={item} />;
+
+  if (loading) {
+    return <ActivityIndicator size="large" />;
+  }
+
+  if (error) {
+    return <Text>{error}</Text>;
+  }
+
   return (
     <SafeAreaView>
-      <FlatList data={productData} renderItem={renderProduct} />
+      <FlatList data={response} renderItem={renderProduct} />
     </SafeAreaView>
   );
 };
